89ca29eb49c798d01f7c09481ed62b02077a4538
aliaksandrzaitsau <35954552+aliaksandrzaitsau@users.noreply.github.com>
2018-03-28 21:30:56 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -48,7 +48,7 @@ job("MNTLAB-${StName}-main-build-job") {
 		}
 	    }
 	}	
-        shell('chmod +x script.sh && ./script.sh >> output.log && tar -czf child${i}-\${StName}-\${BUILD_NUMBER}_dsl_script.tar.gz output.log')
+        shell('./script.sh >> output.log && tar -cf child${i}-\${ChooseBranch}-\${BUILD_NUMBER}_dsl_script.tar.gz output.log')
     }
     publishers { 
 	archiveArtifacts('output.log')
@@ -66,7 +66,7 @@ job("MNTLAB-${StName}-child${i}-build-job") {
         github(git, '$ChooseBranch')
     }
     steps {
-        shell('chmod +x script.sh && ./script.sh >> output.log && tar -czf child${i}-\${StName}-\${BUILD_NUMBER}_dsl_script.tar.gz output.log jobs.groovy script.sh')
+        shell('./script.sh >> output.log && tar -cf child${i}-\${ChooseBranch}-\${BUILD_NUMBER}_dsl_script.tar.gz output.log jobs.groovy script.sh && cp child${i}-\${ChooseBranch}-\${BUILD_NUMBER}_dsl_script.tar.gz ../${mainJob}')
     }
     publishers { 
         archiveArtifacts {
