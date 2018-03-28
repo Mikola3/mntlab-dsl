b73a5d15eba98677769f2dec1fe38c0b4cfb25ad
UladzislauValchkou <tarantino459@gmail.com>
2018-03-28 18:44:44 +0300

    job.groovy

---
+++ job.groovy
@@ -0,0 +1,95 @@
+for (i in 1..4) {
+  job("MNTLAB-uvalchkou-child${i}-build-job") {
+    label('EPBYMINW2471')
+    scm{
+      git { 
+        remote 
+            {
+              github("MNT-Lab/mntlab-dsl", "https") 
+            } 
+        branch("\$branch_child") }
+    }
+   
+    configure {
+    project->
+        project / 'properties' << 'hudson.model.ParametersDefinitionProperty' {
+        parameterDefinitions {
+            'com.cwctravel.hudson.plugins.extended__choice__parameter.ExtendedChoiceParameterDefinition' {
+                name 'branch_child'
+                quoteValue 'false'
+                saveJSONParameterToFile 'false'
+                visibleItemCount '15'
+                type 'PT_SINGLE_SELECT'
+                groovyScript """
+import jenkins.model.*
+def gitURL = "https://github.com/MNT-Lab/mntlab-dsl"
+def command = "git ls-remote -h \$gitURL"
+
+def proc = command.execute()
+ 
+def branches = proc.in.text.readLines().collect { 
+    it.split('/')[2]
+}
+
+branches"""
+                multiSelectDelimiter ','
+                projectName "${jobName}"
+                defaultValue '$branch'
+            }
+        }
+    }
+}
+  steps {
+    shell("""./script.sh > out.txt
+tar czvf \$BUILD_NUMBER-\$JOB_NAME.tar.gz ./out.txt
+mv \$BUILD_NUMBER-\$JOB_NAME.tar.gz /opt/jenkins/master/workspace/\$BUILD_NUMBER-\$JOB_NAME.tar.gz""")
+  }
+  }
+
+}
+
+
+
+
+job("MNTLAB-uvalchkou-main-build-job") {
+  label('EPBYMINW2471')
+    scm{
+      git { 
+        remote 
+            {
+              github("MNT-Lab/mntlab-dsl", "https") 
+            } 
+        branch("\$branch") }
+    }
+    
+    configure {
+    project->
+        project / 'properties' << 'hudson.model.ParametersDefinitionProperty' {
+        parameterDefinitions {
+            'com.cwctravel.hudson.plugins.extended__choice__parameter.ExtendedChoiceParameterDefinition' {
+                name 'child_jobs'
+                quoteValue 'false'
+                saveJSONParameterToFile 'false'
+                visibleItemCount '15'
+                type 'PT_CHECKBOX'
+                value "MNTLAB-uvalchkou-child1-build-job, MNTLAB-uvalchkou-child2-build-job, MNTLAB-uvalchkou-child3-build-job, MNTLAB-uvalchkou-child4-build-job"
+                multiSelectDelimiter ','
+                projectName "${jobName}"
+            }
+        }
+    }
+}
+    	parameters {
+        choiceParam('branch', ['uvalchkou', 'master'], '')
+            } 
+  		steps {
+          downstreamParameterized {
+              trigger('$child_jobs') {
+                 block { 
+                   buildStepFailure("FAILURE")
+                   unstable("FAILURE")
+                   failure("UNSTABLE")
+                 }
+			  parameters {predefinedProp('branch', '$branch')}}}
+    }
+}
