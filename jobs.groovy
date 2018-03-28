6d644b01b2d059769a61212556ebf3e2b1ca647f
NikB97 <35953195+NikB97@users.noreply.github.com>
2018-03-28 22:31:17 +0300

    vers10

--- jobs.groovy
+++ jobs.groovy
@@ -52,11 +52,11 @@ for (i in 1..4) {
       
         steps {
             shell("""bash ./script.sh > output.txt 
-                     tar -cvzf \${BRANCH_NAME}_dsl_script_${i}-${BUILD_NUMBER}.tar.gz output.txt  
-                     cp \${BRANCH_NAME}_dsl_script_${i}-${BUILD_NUMBER}.tar.gz ../MNTLAB-${STUDENT}-main-build-job""")
+                     tar -cvzf \${BRANCH_NAME}_dsl_script_${i}-\${BUILD_NUMBER}.tar.gz output.txt  
+                     cp \${BRANCH_NAME}_dsl_script_${i}-\${BUILD_NUMBER}.tar.gz ../MNTLAB-${STUDENT}-main-build-job""")
         }
         publishers {
-            archiveArtifacts("\${BRANCH_NAME}_dsl_script_${i}-${BUILD_NUMBER}.tar.gz")
+            archiveArtifacts("\${BRANCH_NAME}_dsl_script_${i}-\${BUILD_NUMBER}.tar.gz")
         }
     }
 }
