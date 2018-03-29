559e993f12780df1b5386cdcd2be63e1546594d7
AlexandrSher <30312859+AlexandrSher@users.noreply.github.com>
2018-03-29 10:55:51 +0300

    branch

--- jobs.groovy
+++ jobs.groovy
@@ -29,7 +29,26 @@ name.each {
 job("$it") {
         label ('EPBYMINW6122')
         description("THIS is child")
-        parameters {choiceParam("branch", ["achernak", "master"], "")
+        configure {project ->
+            project / 'properties' << 'hudson.model.ParametersDefinitionProperty' {
+                parameterDefinitions {
+                    'com.cwctravel.hudson.plugins.extended__choice__parameter.ExtendedChoiceParameterDefinition' {
+                        name 'branch'
+                        quoteValue 'false'
+                        saveJSONParameterToFile 'false'
+                        visibleItemCount '5'
+                        type 'PT_SINGLE_SELECT'
+                        groovyScript """
+def gitURL = "https://github.com/MNT-Lab/mntlab-dsl.git"
+def command = "git ls-remote -h \$gitURL"
+def proc = command.execute()
+proc.waitFor()
+def branch = proc.in.text.readLines().collect {
+    it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '')}
+return branch"""
+                        defaultValue 'achernak'
+                        multiSelectDelimiter ','
+                    }                }            }}
         keepDependencies(false)
         scm { git { remote { github("MNT-Lab/mntlab-dsl", "https")}
             branch("\$branch")
